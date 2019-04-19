908d1454d3ff593248b86f43406de4a94d5abe29
Scott Walstead <scott.walstead@procore.com>
2019-04-19 15:47:33 -0700

    Learn UIKit

--- lecture2/ViewController.swift
+++ lecture2/ViewController.swift
@@ -10,11 +10,99 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var blahImage: UIImageView!
+    var titleLabel: UILabel!
+    var bioText: UITextView!
+    var button: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
+
+        blahImage = UIImageView()
+        blahImage.translatesAutoresizingMaskIntoConstraints = false
+        blahImage.image = UIImage(named: "blah")
+        blahImage.clipsToBounds = true
+        blahImage.contentMode = UIView.ContentMode.scaleAspectFill
+        view.addSubview(blahImage)
+
+        titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Well hello there kdfkjajfj kld kfjasdk sfjkjkaskjafjklafkjjklasdkfjsdfj fjsadf jkldfkjlfjakljljkf jklafjklj"
+        titleLabel.textColor = .blue
+        titleLabel.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 0
+        view.addSubview(titleLabel)
+
+        bioText = UITextView()
+        bioText.translatesAutoresizingMaskIntoConstraints = false
+        bioText.text = "hekkfjka jlkfka djkfajkfskjlafsjkkjfdjkfjfskjfkdjjkfjj j j j j jj j  j jfidijnfdnnjjkjk jj jf jkjdsjk fjjfiaimnd ciasfnairingioandingnasi hi ifiadsgnfaoinionewriong;ang; ainnirneinfnai   adf a fae t"
+        bioText.textColor = .yellow
+        bioText.font = UIFont.systemFont(ofSize: 30, weight: .light)
+        bioText.textAlignment = .right
+        bioText.backgroundColor = .black
+        view.addSubview(bioText)
+
+        button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .purple
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 10, weight: .bold)
+        button.titleLabel?.textAlignment = .right
+        button.titleLabel?.text = "Change color"
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        view.addSubview(button)
+
+        setupConstraints()
+//        blahImage.
+
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            blahImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+                blahImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                blahImage.widthAnchor.constraint(equalToConstant: 50),
+                blahImage.heightAnchor.constraint(equalToConstant: 50)
+            ])
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: blahImage.bottomAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            titleLabel.heightAnchor.constraint(lessThanOrEqualToConstant: 5)
+            ])
+
+
+        NSLayoutConstraint.activate([
+            bioText.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
+            bioText.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bioText.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            bioText.widthAnchor.constraint(equalToConstant: 10)
+            bioText.heightAnchor.constraint(equalToConstant: 50)
+//            bioText.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+            ])
+
+        NSLayoutConstraint.activate([
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            button.centerXAnchor.constraint(equalTo: bioText.centerXAnchor),
+            button.heightAnchor.constraint(equalToConstant: 30),
+            button.widthAnchor.constraint(equalToConstant: 100)
+            ])
+    }
+
+    @objc func buttonTapped() {
+        print("tapped button")
+        if bioText.backgroundColor == .black {
+            bioText.backgroundColor = .blue
+        } else {
+            bioText.backgroundColor = .black
+        }
+
+    }
 
 }
 
